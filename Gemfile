d3b013d2730e3edba5f02c766a12b59bb35eb154
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-12-03 20:08:57 +0100

    changed to solargraph and rubocop

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
-gem "syntax_tree"
 gem "bcrypt"
+gem 'solargraph'
+gem 'rubocop'
--- Ruby OOP/password.rb
+++ Ruby OOP/password.rb
@@ -1,11 +1,9 @@
-require "bcrypt"
+require 'bcrypt'
 
 def create_hashed_password(password)
   BCrypt::Password.create(password)
 end
 
-
 def create_new_hash(given_hash)
   BCrypt::Password.new(given_hash)
 end
-
