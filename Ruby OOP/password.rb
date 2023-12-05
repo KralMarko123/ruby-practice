4252e27385f14a25fa9c7a0483e9c1e43f4b8224
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-12-05 14:47:39 +0100

    modified password to be a module

--- Ruby OOP/password.rb
+++ Ruby OOP/password.rb
@@ -1,9 +1,11 @@
 require 'bcrypt'
 
-def create_hashed_password(password)
-  BCrypt::Password.create(password)
-end
+module BCryptPasswordGenerator
+  def self.create_hashed_password(password)
+    BCrypt::Password.create(password)
+  end
 
-def create_new_hash(given_hash)
-  BCrypt::Password.new(given_hash)
+  def self.create_new_hash(given_hash)
+    BCrypt::Password.new(given_hash)
+  end
 end
