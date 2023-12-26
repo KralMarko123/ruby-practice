376b2aed954c2ba39e73aed5a9bfa9e360c2f12a
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-12-26 13:12:34 +0100

    added prettier and validations

--- apps/blog-app/app/models/article.rb
+++ apps/blog-app/app/models/article.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
   def initialize(title, description)
     @title = title,
-             @description = description
+    @description = description  
   end
 end
+
