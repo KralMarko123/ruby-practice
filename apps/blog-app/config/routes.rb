52d32246729a4bf2078343ff1608f1d7f92fda73
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-12-27 14:40:39 +0100

    index articles

--- apps/blog-app/config/routes.rb
+++ apps/blog-app/config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-
   root 'pages#home'
+
+  # PAGES
   get '/about' => 'pages#about'
-  resources :articles, only: [:show]
+
+  # ARTICLES
+  resources :articles, only: [:show, :index]
 end
